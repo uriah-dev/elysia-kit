a2bf0e6f5f13273ccd13fb6a264317062a4b3731
uriah-dev <otch.dev@gmail.com>
2025-12-27 21:17:31 +0000

    Update bun.lock for bun-types and elysia versions; modify tsconfig.json for module resolution; refactor index.ts to use environment variable for port.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { app } from "./app/_app";
+import { env } from "./env";
 
-app.get("/", () => "Hello Elysia").listen(3000);
+app.get("/", () => "Hello Elysia").listen(env.PORT);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
