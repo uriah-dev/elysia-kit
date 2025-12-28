ade940a168aed8c2d27f1df206782ec6220215b6
uriah-dev <otch.dev@gmail.com>
2025-12-28 12:24:47 +0000

    Refactor server initialization to streamline port configuration; utilize buildServiceUrl for dynamic logging of server address.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
 import { server } from "@app/_app";
 import { env } from "@src/env";
 import { logger } from "./lib/logger";
-
-server.listen(env.APP_PORT);
-
-const config = {
-  hostname: server.server?.hostname,
-  port: server.server?.port,
-};
-
-logger.info(`🦊 Server is running at ${config?.hostname}:${config?.port}`);
+import { buildServiceUrl } from "./lib/utils";
 
 export type Server = typeof server;
+
+const PORT = env.APP_PORT;
+server.listen(PORT);
+
+logger.info(`🦊 Server is running at ${buildServiceUrl(PORT)}`);
