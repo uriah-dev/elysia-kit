934b2b9218565996084b278c97406a070ae100f0
uriah-dev <otch.dev@gmail.com>
2025-12-30 09:07:41 +0000

    feat: Configure Vercel deployment with Bun, update API entrypoint, and enable Bun path alias resolution.

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,5 @@
-import { server } from "../src/app/_app";
+// Vercel serverless function entrypoint
+// Note: Using relative imports to avoid path alias issues on Vercel
+import { app } from "../src/app/_app";
 
-export default server;
+export default app;
